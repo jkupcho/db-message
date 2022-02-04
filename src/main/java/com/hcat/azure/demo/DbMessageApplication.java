68855a2e58c9f94cda0b787f35e8e5f335dc6769
Jonathan <jonathan.kupcho@gmail.com>
2022-02-04 09:57:16 -0600

    message done

--- src/main/java/com/hcat/azure/demo/DbMessageApplication.java
+++ src/main/java/com/hcat/azure/demo/DbMessageApplication.java
@@ -2,12 +2,36 @@ package com.hcat.azure.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 public class DbMessageApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(DbMessageApplication.class, args);
+    }
+
+    private class HelloMessage {
+        private final String message;
+
+        private HelloMessage(String message) {
+            this.message = message;
+        }
+
+        public String getMessage() {
+            return message;
+        }
+    }
+
+    @RestController
+    public class HelloController {
+
+        @GetMapping("/hello")
+        public HelloMessage hello() {
+            return new HelloMessage("Hello from Spring Boot!");
+        }
+
     }
 
 }
