ad000795dc819b311279e5325be917f06fdc414f
Jonathan <jonathan.kupcho@gmail.com>
2022-02-04 09:51:24 -0600

    initial commit

---
+++ src/main/java/com/hcat/azure/demo/DbMessageApplication.java
@@ -0,0 +1,13 @@
+package com.hcat.azure.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DbMessageApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DbMessageApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/hcat/azure/demo/DbMessageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hcat.azure.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DbMessageApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
